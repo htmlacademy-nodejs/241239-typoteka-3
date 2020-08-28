85775dae2f60f7d2314f90585055af1e79741a34
viktor kan <viktorkan9@gmail.com>
2020-08-28 13:33:36 +0300

    add service for mock

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const generate = require(`./generate`);
+const help = require(`./help`);
+const version = require(`./version`);
 
 const Cli = {
-  [generate.name]: generate
+  [generate.name]: generate,
+  [help.name]: help,
+  [version.name]: version
 };
 
 module.exports = {

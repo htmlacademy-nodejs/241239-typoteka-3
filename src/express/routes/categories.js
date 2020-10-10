705dd971c1c783abeab6313ab6a7012d2b824e78
viktor kan <viktorkan9@gmail.com>
2020-10-10 16:34:51 +0300

    add pug templates

--- src/express/routes/categories.js
+++ src/express/routes/categories.js
@@ -2,7 +2,7 @@ const {Router} = require(`express`);
 
 const categoriesRoutes = new Router();
 
-categoriesRoutes.get(`/`, (req, res) => res.send(`/categories`));
+categoriesRoutes.get(`/`, (req, res) => res.render(`all-categories`));
 
 
 
--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -2,8 +2,8 @@ const {Router} = require(`express`);
 
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
-mainRouter.get(`/login`, (req, res) => res.send(`/login`));
+mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
+mainRouter.get(`/login`, (req, res) => res.render(`login`));
 
 module.exports = mainRouter;
